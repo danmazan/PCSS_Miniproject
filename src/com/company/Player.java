c04f2a75555d0230817276b75faa9e4c62b85038
danmazan <dan.mazan3@gmail.com>
2019-10-03 13:35:05 +0200

    created Player class with empty bodies

---
+++ src/com/company/Player.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Player {
+
+    public Player(){
+        boolean viborg = false;
+    }
+    public void estimate(int amount, int number){
+
+    }
+
+    public void increase(int amount, int number){
+
+    }
+
+    public void lift(){
+
+    }
+
+    public void looseDie(){
+
+    }
+
+}
