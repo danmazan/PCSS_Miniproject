562e09bec0c257e8ccf584b7c6d8317e97c970c2
Connie RageCage <43566860+Alex696969@users.noreply.github.com>
2019-10-28 16:05:13 +0100

    Moving dice class from client to server

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -11,36 +11,15 @@ import java.io.IOException;
 import java.net.Socket;
 
 
-public class Player extends Dice {
+public class Player {
 
     Scanner keyboard = new Scanner(System.in);
     public String name;
-    boolean viborg = false;
-    ArrayList<Dice> myDice = new ArrayList<Dice>();
     int betAmount = 0;
     int betNumber = 0;
 
     //constructor
     public Player(){
-        //this.name = name;
-        myDice.add(new Dice());
-        myDice.add(new Dice());
-        myDice.add(new Dice());
-        myDice.add(new Dice());
-    }
-
-    public void shuffle(){
-        for (int i=0; i<myDice.size(); i++){
-            myDice.get(i).roll();
-
-            }
-        }
-
-
-    public void printDice(){
-        System.out.println("Your dice: ");
-        myDice.forEach( (i) -> System.out.print(i.value + ", "));
-        System.out.println(" ");
     }
 
     //if increase function is not okay with previous player (server stuff)
@@ -53,19 +32,4 @@ public class Player extends Dice {
             betNumber  = keyboard.nextInt();
         }
     }
-
-
-    public void lift(){
-    /*
-    count all relevant dice (count 6s eg0)rint all dice as:
-    "Player1: 2, 6, 5, 4"
-    "Player2: 3,3,6,5"
-    ...
-     */
-    }
-
-    public void looseDie(){
-    myDice.remove(myDice.size()-1);
-    }
-
 }
